37d0158c66b31b206fca393e5c000e6c69b94db2
--get <107877150+Spinozanilast@users.noreply.github.com>
2026-05-13 17:46:10 +0300

    migrate tailwind+vanilla css to tailwind full and refactor some complex components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-export default [
-    ...eslintPluginAstro.configs.recommended,
-    {
-        rules: {
-        }
-    }
-];
+import { defineConfig } from "eslint/config";
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+    eslintPluginAstro.configs.recommended,
+    tseslint.config.recommended,
+);
