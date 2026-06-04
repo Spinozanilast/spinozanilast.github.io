f90fd46cd7482372a742726cbe12bccb932eb9af
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2026-06-04 09:00:01 +0300

    feat | cd: add html inlining for popup typewriters and add github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://spinozanilast.github.io",
+  base: "/personal",
+
+  integrations: [],
 
   vite: {
     plugins: [tailwindcss()]
