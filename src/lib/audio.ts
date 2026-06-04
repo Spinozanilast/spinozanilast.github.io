d33d3156dd8e5e4e474e4991d4eba0928c232587
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2026-06-04 09:00:02 +0300

    feat(home): end make keyboard for now

---
+++ src/lib/audio.ts
@@ -0,0 +1,39 @@
+class AudioPlayer {
+    #cachedAudioElements: Map<string, HTMLAudioElement> = new Map();
+
+    createAudioPlayer(src: string) {
+        const audio = new Audio(src);
+        this.#cachedAudioElements.set(src, audio);
+    }
+
+    #prepareAudio(src: string): HTMLAudioElement {
+        const audio = this.#cachedAudioElements.get(src);
+
+        if (!audio) {
+            console.log("First create audio player");
+            throw new Error("Audio player not found");
+        }
+
+        return audio;
+    }
+
+    playAudio(src: string, deleteAfterPlay: boolean = false) {
+        const audioElem = this.#prepareAudio(src);
+
+        audioElem.currentTime = 0;
+        audioElem.play();
+
+        if (deleteAfterPlay) {
+            audioElem?.addEventListener("ended", () => {
+                audioElem.remove();
+                this.#cachedAudioElements.delete(src);
+            });
+        }
+    }
+
+    playAudioWithDeleteAfter(src: string) {
+        this.playAudio(src, true);
+    }
+}
+
+export default new AudioPlayer();
