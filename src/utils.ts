7a7e292be867b091ddeeeffa78a5b6c0318a4588
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2026-06-04 09:00:01 +0300

    fix: change popup layout on small devices

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function addEventListeners<EventsName extends keyof HTMLElementEventMap>(
+    node: HTMLElement,
+    eventsNames: EventsName[],
+    callback: EventListener
+) {
+    for (const eventName of eventsNames) {
+        node.addEventListener(eventName, callback);
+    }
+}
