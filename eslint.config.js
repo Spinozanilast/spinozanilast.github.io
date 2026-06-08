75803b886a338a072f7c2b7d21ea48076376f72e
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2026-06-08 19:03:53 +0300

    feat: fix layouts in home page to work correct)

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default [...eslintPluginAstro.configs.recommended, ...tseslint.configs.recommended];
--- eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from "eslint/config";
-import eslintPluginAstro from "eslint-plugin-astro";
-import tseslint from "typescript-eslint";
-
-export default defineConfig(eslintPluginAstro.configs.recommended, tseslint.configs.recommended);
