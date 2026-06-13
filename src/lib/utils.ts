be7934a869556e484a407489022144fc612dc980
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2026-06-13 19:51:32 +0300

    fix(home): remove flex class from sections wrapper that appeared

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,28 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Adds event listeners to a node for multiple events
+ *
+ * @param node The node to add event listeners to
+ * @param eventsNames The names of the events to listen for
+ * @param callback The callback function to execute when an event is triggered
+ */
 export function addEventListeners<EventsName extends keyof HTMLElementEventMap>(node: HTMLElement, eventsNames: EventsName[], callback: EventListener) {
   for (const eventName of eventsNames) {
     node.addEventListener(eventName, callback);
+  }
+}
+
+/**
+ * Utility function to change the hide state of an element
+ *
+ * @param elementId The ID of the element to toggle
+ * @param hide Whether to hide or show the element
+ */
+export function changeElementHideState(elementId: string, hide: boolean) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.classList.toggle("hidden", hide);
   }
 }
